844eb393472175a572b3c5c4ca4641775be0fc15
soekeeee <zainiddin.d.s@gmail.com>
2026-06-06 18:44:34 +0200

    Add browser headers to requests to avoid 404 errors

--- anikoto.en.bangumi.js
+++ anikoto.en.bangumi.js
@@ -1,6 +1,6 @@
 // ==MiruExtension==
 // @name        AniKoto
-// @version     v0.0.4
+// @version     v0.0.5
 // @author      zaini+copilot
 // @lang        en
 // @icon        https://anikototv.to/favicon.ico
@@ -22,6 +22,12 @@ export default class extends Extension {
     const res = await this.request({
       url,
       method: 'GET',
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Referer': this.baseUrl,
+      },
     });
 
     const parser = new DOMParser();
